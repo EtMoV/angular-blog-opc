0b6b5f6a0a14298bfb118d4dc9259a5f83575dde
Etienne Motara <motaraetiennepro@gmail.com>
2018-09-20 10:49:51 +0200

    Application blog Angular OPC 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+import { Post } from "./Post";
+import { loremIpsum } from "./loremIpsum";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-blog-opc';
+  title = "Le blog OPC sur Angular";
+  posts = [
+    new Post("Mon premier post", loremIpsum),
+    new Post("Mon deuxième post", loremIpsum),
+    new Post("Encore un post", loremIpsum)
+  ];
 }
